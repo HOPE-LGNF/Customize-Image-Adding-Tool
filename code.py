ad2b07e56a9891bf2f54f609441a449116314ce3
HOPE-LGNF <159258343+HOPE-LGNF@users.noreply.github.com>
2024-06-16 22:08:58 +0800

    ...update new UI ...
    1.Now you can choose your local images to add.
    2.Fixed issues that may have been caused by changing the suffix forcefully.
    3.Updated UI

--- code.py
+++ code.py
@@ -9,9 +9,84 @@ from UI import Ui_Form
 from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox, QFileDialog
 from PyQt5.QtCore import QUrl, QRegExp
 from PyQt5.QtGui import QRegExpValidator
+from urllib.parse import urlparse
+from PIL import Image
 import requests
 import sys, os
 import webbrowser
+
+
+def copy_file(old_path: str, new_path: str, is_byte: bool = False, encoding: str = "utf-8",
+              allow_overwrite_file: bool = False) -> None or FileExistsError:
+    """
+    基于os模块
+    复制文件的函数，将源文件的内容复制到目标文件中。支持文本文件和二进制文件的复制。
+
+    :param old_path: 源文件路径(填写文件名)。
+    :param new_path: 目标文件路径(填写文件名)。
+    :param is_byte: 是否以二进制模式复制文件，默认为False。
+    :param encoding: 编码格式，默认为"utf-8"。
+    :param allow_overwrite_file: 是否允许覆盖目标文件，默认为False。
+
+    :raises FileExistsError: 如果源文件或目标文件已存在且不允许覆盖文件，则引发此异常。
+    :return: None
+    """
+    try:  # 检测是否有os模块
+        _AsxzwdecSecavased_soa_jJSHNbnmxznJSnmLKS_vqlr = os.name
+    except NameError:
+        import os
+        print("imported")
+
+    if os.path.exists(new_path):
+        allow = -1
+    elif not os.path.exists(old_path):
+        allow = -2
+    else:
+        allow = 1
+    if allow_overwrite_file:
+        allow = 1
+    match allow:
+        case 1:
+            with open(old_path, f"r{'b' if is_byte else ''}", encoding=None if is_byte else encoding) as f:
+                _temp = f.read()
+            with open(new_path, f"w{'b' if is_byte else ''}", encoding=None if is_byte else encoding) as i:
+                i.write(_temp)
+        case -1:
+            raise FileExistsError("[WinError 114514] 目标文件已存在")
+        case -2:
+            raise FileExistsError("[WinError 1919810] 源文件路径无效")
+
+
+def trans_image_suffix_into_PNG(path: str, newname=None) -> None:
+    """
+    将图片文件转换为PNG格式文件。
+    需要from PIL import Image
+
+    :param path: 需要转换的图片文件的路径。
+    :param newname: PNG文件的新名称。如果未提供，默认为原始文件名附带PNG扩展名。
+
+    :return: None
+
+    示例:trans_image_suffix_into_PNG('/path/to/image.jpg', 'new_image.png')
+    """
+    dir_, name = os.path.split(path)
+    if newname is None:
+        newname = name.split('.')[0] + '.png'  # new name for png file
+    Image.open(path).save(os.path.join(dir_, newname))
+
+
+def filename_catcher(url: str) -> tuple[str, str]:
+    """
+    从给定的URL中提取文件名和文件后缀。
+    (需要from urllib.parse import urlparse导入)
+
+    :param url: 包含文件名的URL字符串。
+    :return: tuple[Filename, Filesuffix] 返回提取的文件名和文件后缀。
+    """
+    a = urlparse(url)
+    file_name = os.path.basename(a.path)
+    _, file_suffix = os.path.splitext(file_name)
+    return file_name, file_suffix
 
 
 class MainWindow(QWidget):
@@ -23,38 +98,22 @@ class MainWindow(QWidget):
 
         validator = QRegExpValidator(QRegExp('[^\\\\/:*?"<>|\r\n]+$'))
         self.ui.hint_input.setValidator(validator)
-        self.ui.savefilenameEdit.setValidator(validator)
-        self.imagePath, self.DFP, self.savePath = "", "", ""
+        self.imagePath = ""
+        self.mode = 0
 
     def initwindget(self):
-        self.ui.download_file.clicked.connect(self.Download)
-        self.ui.downloadfilepathEdit.textChanged.connect(self.Auto_Change_Filename)
+        self.ui.add_file.clicked.connect(self.Add)
         self.ui.fandomLinkButton_2.clicked.connect(self.Link_Fandom)
         self.ui.esixLinkButton.clicked.connect(self.Link_e621)
         self.ui.preview_btn.clicked.connect(self.change_webviewer_url)
-        self.ui.open_file_dir.clicked.connect(self.open_folder)
-        self.ui.chooseimage.clicked.connect(self.imageOpenDialog)
-        self.ui.pushButton.clicked.connect(self.Finally_Add)
-        self.ui.choosesavepath.clicked.connect(self.folderOpenDialog)
+        self.ui.mode_changer_combobox.currentIndexChanged.connect(self.change_mode)
+        self.ui.choose_img_file_path_btn.clicked.connect(self.imageOpenDialog)
 
     def change_webviewer_url(self):
         try:
-            self.ui.ImagewebPreViewer.load(QUrl(self.DFP))
+            self.ui.ImagewebPreViewer.load(QUrl(self.ui.downloadfilepathEdit.text()))
         except Exception as e:
-            print(e)
-            self.Dialogue("Error!", e, 2)
-
-    def Auto_Change_Filename(self):
-        """
-        a: https://picx.zhimg.com/70/v2-23a56248e4148780e5a15b7f4ea77948_1440w.awebp
-        a: https://static.wikia.nocookie.net/changed1449/images/9/93/179.png/revision/latest?cb=20220115225401
-        a: https://www.baidu.com/favicon.ico
-        """
-        self.DFP = self.ui.downloadfilepathEdit.text()
-        link = self.ui.downloadfilepathEdit.text()
-        filename = "/".join(link.split("/")[3:]).split(".")[0].split("/")[-1] + "." + \
-                   "/".join(link.split("/")[3:]).split(".")[-1].split("/")[0]
-        self.ui.savefilenameEdit.setText(filename)
+            self.Dialogue("出问题了!", e, 2)
 
     @staticmethod
     def Link_Fandom():
@@ -64,40 +123,34 @@ class MainWindow(QWidget):
     def Link_e621():
         webbrowser.open("https://yandex.com/images")
 
-    def Download(self):
-        savedir = self.ui.savepathEdit.text()
-        savefilename = self.ui.savefilenameEdit.text()
-        print(savefilename)
-        try:
-            if self.ui.downloadfilepathEdit.text() == "":
-                raise ValueError("未输入下载地址!")
-            if savedir == "":
-                raise ValueError("未输入保存文件夹路径!")
-            elif not os.path.exists(f"{savedir}"):
-                raise FileNotFoundError("文件夹不存在!")
-            if savefilename == "":
-                raise ValueError("未输入保存文件名!")
-            res = requests.get(self.ui.downloadfilepathEdit.text())
-        except Exception as e:
-            self.Dialogue("出问题了!", f"错误原因: {str(e)}", 2)
-        else:
+    def change_mode(self):
+        # todo 国际化
+        self.mode = self.ui.mode_changer_combobox.currentIndex()
+        match self.mode:
+            case 0:
+                self.ui.downloadfilepathEdit.setText("")
+                print("0case")
+                self.ui.choose_img_file_path_btn.setEnabled(False)
+                # self.ui.preview_btn.setEnabled(True)
+                self.ui.label_3.setText("下载地址")
+                self.ui.add_file.setText("下载并添加")
+                self.ui.downloadfilepathEdit.setPlaceholderText("链接必须为以http://或https://开头的网址,部分网址可能无法下载")
+            case 1:
+                self.ui.downloadfilepathEdit.setText("")
+                print("1case")
+                self.ui.choose_img_file_path_btn.setEnabled(True)
+                # self.ui.preview_btn.setEnabled(False)
+                self.ui.label_3.setText("本地图片")
+                self.ui.add_file.setText("添加")
+                self.ui.downloadfilepathEdit.setPlaceholderText("需填写绝对路径")
+            case _:
+                print(self.mode)
 
-            print("OK")
-            if savedir[-1] != '/':
-                savedir += "\\"
-            with open(f'{savedir}{savefilename}', 'wb') as file:
-                file.write(res.content)
-            self.Dialogue("喜报", f"{savefilename}下载完成", 0)
-
-    def open_folder(self):
-        print(os.path.exists(self.ui.savepathEdit.text()))
-        if self.ui.savepathEdit.text() == "":
-            pass
-        elif not os.path.exists(self.ui.savepathEdit.text()):
-            print("目录不存在")
-        else:
-            print(type(self.ui.savepathEdit.text()))
-            os.startfile(f"{self.ui.savepathEdit.text()}")
+    def imageOpenDialog(self):
+        fname, _ = QFileDialog.getOpenFileName(self, "打开", f"{os.path.join(os.environ['USERPROFILE'],'Pictures')}", "PNG图片文件(*.png)")
+        if fname:
+            self.imagePath = fname
+        self.ui.downloadfilepathEdit.setText(self.imagePath)  # mode 2
 
     def Dialogue(self, title, text, mode=0):
         """
@@ -115,35 +168,60 @@ class MainWindow(QWidget):
         elif mode == 3:
             QMessageBox.question(self, title, text)
 
-    def imageOpenDialog(self):
-        fname, _ = QFileDialog.getOpenFileName(self, "打开", "C:\\", "图片文件(*.png *.jpg)")
-        if fname:
-            self.imagePath = fname
-        self.ui.inagefilepathEdit.setText(self.imagePath)
+    def Add(self):
+        savefilename = self.ui.hint_input.text()
+        try:
+            # 检查 PICTURES_CARD 文件夹
+            if not os.path.exists(f"{os.getcwd()}\\PICTURES_CARD"):
+                os.mkdir("PICTURES_CARD")
+                self.Dialogue("信息", "未检测到PICTURES_CARD文件夹,已创建")
 
-    def folderOpenDialog(self):
-        fname = QFileDialog.getExistingDirectory(self, "打开", "C:\\")
-        if fname:
-            self.savePath = fname
-        self.ui.savepathEdit.setText(self.savePath)
+            # 检查 hints.txt
+            if not os.path.exists("hints.txt"):
+                self.Dialogue("通知", "未检测到hint文件,已创建", 0)
+                open("hints.txt", "w", encoding="utf-8").close()
 
-    def Finally_Add(self):
-        if self.ui.inagefilepathEdit.text():
-            if self.ui.hint_input.text():
-                if os.path.exists(f"{self.ui.inagefilepathEdit.text()}"):
-                    if os.path.exists("hint.txt"):
-                        with open("hint.txt", "a", encoding="utf-8") as f:
-                            f.write(f"\n{self.ui.hint_input.text()}")
-                    else:
-                        with open("hint.txt", "w", encoding="utf-8") as f:
-                            f.write(f"{self.ui.hint_input.text()}")
+            with open("hints.txt", "r", encoding="utf-8") as f:
+                lines = f.readlines()
+            for i in lines:
+                if savefilename == i:
+                    raise ValueError("此hint条目已存在")
 
-                    with open(self.ui.inagefilepathEdit.text(), "rb") as f:
-                        bdata = f.read()
-                    with open(f"{os.path.dirname(__file__)}\\PICTURES_CARD\\{self.ui.hint_input.text()}.{os.path.basename(self.ui.inagefilepathEdit.text()).split('.')[-1]}", "wb") as g:
-                        g.write(bdata)
-                else:
-                    self.Dialogue("出问题了", "图片路径无效", 2)
+            if self.ui.downloadfilepathEdit.text() == "":
+                raise ValueError("未输入地址!")
+            if self.ui.hint_input.text() == "":
+                raise ValueError("未输入“你知道吗”内容!")
+
+            # 匹配模式
+            match self.mode:
+                case 0:  # 下载模式
+                    res = requests.get(self.ui.downloadfilepathEdit.text())
+                    filename:str = filename_catcher(self.ui.downloadfilepathEdit.text())[0]  # filename包含后缀
+                    if not os.path.exists(f"{os.getenv('TEMP')}\\ImageAddingTool\\"):
+                        os.makedirs(f"{os.getenv('TEMP')}\\ImageAddingTool\\")
+                    with open(f"{os.getenv('TEMP')}\\ImageAddingTool\\{filename}", 'wb') as file:  # download to TEMP
+                        file.write(res.content)
+
+                    self.Dialogue("喜报", f"{savefilename}下载完成", 0)
+
+                    trans_image_suffix_into_PNG(f"{os.getenv('TEMP')}\\ImageAddingTool\\{filename}")  # change imgsuffix
+
+                    copy_file(f"{os.getenv('TEMP')}\\ImageAddingTool\\{os.path.splitext(filename)[0]}.png",  # todo attention
+                              f'{os.getcwd()}\\PICTURES_CARD\\{savefilename}.png', is_byte=True)
+                    print(f'{os.getcwd()}\\PICTURES_CARD\\{savefilename}.png')
+                case 1:  # 本地模式
+                    if not os.path.isabs(self.imagePath):
+                        raise ValueError("路径不是绝对路径")
+                    copy_file(self.imagePath, f"{os.getcwd()}\\PICTURES_CARD\\{savefilename}.png", is_byte=True)
+                case _:
+                    raise NotImplementedError("more modes")
+
+        except Exception as e:
+            self.Dialogue("出问题了!", f"错误原因: {str(e)}", 2)
+        else:
+            with open("hints.txt", "a", encoding="utf-8") as f:
+                f.write(f"\n{self.ui.hint_input.text()}")
+            self.Dialogue("喜报", f"{savefilename}添加完成", 0)
 
 
 if __name__ == '__main__':
@@ -151,3 +229,7 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+
+
+
